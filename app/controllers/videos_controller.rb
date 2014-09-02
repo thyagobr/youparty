8f130ba612b18da2bb4af0bffe12e5cfc7d6bc74
Thyago Barbosa Rodrigues <thyagobr@gmail.com>
2014-09-02 16:27:17 -0300

    Updates

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,67 +2,6 @@ require 'google/api_client'
 require 'oauth_util'
 
 class VideosController < ApplicationController
-  before_action :set_video, only: [:show, :edit, :update, :destroy]
-
-  # GET /videos
-  # GET /videos.json
-  def index
-    @videos = Video.all
-  end
-
-  # GET /videos/1
-  # GET /videos/1.json
-  def show
-  end
-
-  # GET /videos/new
-  def new
-    @video = Video.new
-  end
-
-  # GET /videos/1/edit
-  def edit
-  end
-
-  # POST /videos
-  # POST /videos.json
-  def create
-    @video = Video.new(video_params)
-
-    respond_to do |format|
-      if @video.save
-        format.html { redirect_to @video, notice: 'Video was successfully created.' }
-        format.json { render :show, status: :created, location: @video }
-      else
-        format.html { render :new }
-        format.json { render json: @video.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /videos/1
-  # PATCH/PUT /videos/1.json
-  def update
-    respond_to do |format|
-      if @video.update(video_params)
-        format.html { redirect_to @video, notice: 'Video was successfully updated.' }
-        format.json { render :show, status: :ok, location: @video }
-      else
-        format.html { render :edit }
-        format.json { render json: @video.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /videos/1
-  # DELETE /videos/1.json
-  def destroy
-    @video.destroy
-    respond_to do |format|
-      format.html { redirect_to videos_url, notice: 'Video was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   def callback
     if params[:code]
@@ -91,7 +30,7 @@ class VideosController < ApplicationController
         :client_id => user_credentials.client_id,
         :client_secret => user_credentials.client_secret,
         :redirect_uri => user_credentials.redirect_uris.first,
-        :scope => 'https://www.googleapis.com/auth/youtube'
+        :scope => ['https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/plus']
       )
       auhtorization.code = params[:code]
       auhtorization.fetch_access_token!
